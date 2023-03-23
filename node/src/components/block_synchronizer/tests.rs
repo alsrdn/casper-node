3043f57f129c3a785cbfae343e9df6026f7ce7e7
Alexandru Sardan <alexandru@casperlabs.io>
2023-03-23 18:29:50 +0000

    block_synchronizer: tests
    Signed-off-by: Alexandru Sardan <alexandru@casperlabs.io>

--- node/src/components/block_synchronizer/tests.rs
+++ node/src/components/block_synchronizer/tests.rs
@@ -1,5 +1,6 @@
 pub(crate) mod test_utils;
 
+use assert_matches::assert_matches;
 use std::{collections::HashSet, iter, rc::Rc, time::Duration};
 
 use casper_types::testing::TestRng;
@@ -90,6 +91,26 @@ fn check_sync_global_state_event(event: MockReactorEvent, block: &Block) {
         global_sync_request.state_root_hash,
         *block.state_root_hash()
     );
+}
+
+// Calls need_next for the block_synchronizer and processes the effects resulted returning a list of
+// the new events that were generated
+async fn need_next(
+    rng: &mut TestRng,
+    reactor: &MockReactor,
+    block_synchronizer: &mut BlockSynchronizer,
+    num_expected_events: usize,
+) -> Vec<MockReactorEvent> {
+    let mut effects = block_synchronizer.need_next(reactor.effect_builder(), rng);
+    assert_eq!(effects.len(), num_expected_events);
+
+    let mut events = Vec::new();
+    for effect in effects.drain(0..) {
+        tokio::spawn(async move { effect.await });
+        let event = reactor.crank().await;
+        events.push(event);
+    }
+    events
 }
 
 #[tokio::test]
@@ -207,5 +228,154 @@ async fn global_state_sync_wont_stall_with_bad_peers() {
             .unwrap()
             .peer_list()
             .is_peer_unreliable(peer));
+    }
+}
+
+#[test]
+fn duplicate_register_block_not_allowed_if_builder_is_not_failed() {
+    let mut rng = TestRng::new();
+
+    // Set up a random block that we will use to test synchronization
+    let block = Block::random(&mut rng);
+
+    // Set up a validator matrix for the era in which our test block was created
+    let mut validator_matrix = ValidatorMatrix::new_with_validator(ALICE_SECRET_KEY.clone());
+    validator_matrix.register_validator_weights(
+        block.header().era_id(),
+        iter::once((ALICE_PUBLIC_KEY.clone(), 100.into())).collect(),
+    );
+
+    // Create a block synchronizer with a maximum of 5 simultaneous peers
+    let mut block_synchronizer = BlockSynchronizer::new(
+        Config::default(),
+        Arc::new(Chainspec::random(&mut rng)),
+        5,
+        validator_matrix,
+        prometheus::default_registry(),
+    )
+    .unwrap();
+
+    // Register block for historical sync
+    assert!(block_synchronizer.register_block_by_hash(*block.hash(), false, true));
+    assert!(block_synchronizer.forward.is_some()); // we only get global state on historical sync
+
+    // Registering the block again should not be allowed until the sync finishes
+    assert!(!block_synchronizer.register_block_by_hash(*block.hash(), false, true));
+
+    // Trying to register a different block should replace the old one
+    let new_block = Block::random(&mut rng);
+    assert!(block_synchronizer.register_block_by_hash(*new_block.hash(), false, true));
+    assert_eq!(
+        block_synchronizer.forward.unwrap().block_hash(),
+        *new_block.hash()
+    );
+}
+
+#[tokio::test]
+async fn historical_sync_gets_peers_form_both_connected_peers_and_accumulator() {
+    const MAX_SIMULTANEOUS_PEERS: u32 = 5;
+    let mut rng = TestRng::new();
+    let mock_reactor = MockReactor::new();
+
+    // Set up a random block that we will use to test synchronization
+    let block = Block::random(&mut rng);
+
+    // Set up a validator matrix for the era in which our test block was created
+    let mut validator_matrix = ValidatorMatrix::new_with_validator(ALICE_SECRET_KEY.clone());
+    validator_matrix.register_validator_weights(
+        block.header().era_id(),
+        iter::once((ALICE_PUBLIC_KEY.clone(), 100.into())).collect(),
+    );
+
+    // Create a block synchronizer with a maximum of 5 simultaneous peers
+    let mut block_synchronizer = BlockSynchronizer::new(
+        Config::default(),
+        Arc::new(Chainspec::random(&mut rng)),
+        MAX_SIMULTANEOUS_PEERS,
+        validator_matrix,
+        prometheus::default_registry(),
+    )
+    .unwrap();
+
+    // Register block for historical sync
+    assert!(block_synchronizer.register_block_by_hash(*block.hash(), true, true));
+    assert!(block_synchronizer.historical.is_some()); // we only get global state on historical sync
+
+    let events = need_next(&mut rng, &mock_reactor, &mut block_synchronizer, 2).await;
+
+    // The first thing the synchronizer should do is get peers.
+    // For the historical flow, the synchronizer will get a random sampling of the connected
+    // peers and also ask the accumulator to provide peers from which it has received information
+    // for the block that is being synchronized.
+    assert_matches!(
+        events[0],
+        MockReactorEvent::NetworkInfoRequest(NetworkInfoRequest::FullyConnectedPeers {
+            count,
+            ..
+        }) if count == MAX_SIMULTANEOUS_PEERS as usize
+    );
+
+    assert_matches!(
+        events[1],
+        MockReactorEvent::BlockAccumulatorRequest(BlockAccumulatorRequest::GetPeersForBlock {
+            block_hash,
+            ..
+        }) if block_hash == *block.hash()
+    )
+}
+
+#[tokio::test]
+async fn sync_starts_with_header_fetch() {
+    const MAX_SIMULTANEOUS_PEERS: u32 = 5;
+    let mut rng = TestRng::new();
+    let mock_reactor = MockReactor::new();
+
+    // Set up a random block that we will use to test synchronization
+    let block = Block::random(&mut rng);
+
+    // Set up a validator matrix for the era in which our test block was created
+    let mut validator_matrix = ValidatorMatrix::new_with_validator(ALICE_SECRET_KEY.clone());
+    validator_matrix.register_validator_weights(
+        block.header().era_id(),
+        iter::once((ALICE_PUBLIC_KEY.clone(), 100.into())).collect(),
+    );
+
+    // Create a block synchronizer with a maximum of 5 simultaneous peers
+    let mut block_synchronizer = BlockSynchronizer::new(
+        Config::default(),
+        Arc::new(Chainspec::random(&mut rng)),
+        MAX_SIMULTANEOUS_PEERS,
+        validator_matrix,
+        prometheus::default_registry(),
+    )
+    .unwrap();
+
+    // Register block for fwd sync
+    assert!(block_synchronizer.register_block_by_hash(*block.hash(), false, true));
+    assert!(block_synchronizer.forward.is_some());
+    // Generate peers and register them
+    let num_peers = rng.gen_range(10..20);
+    let peers: Vec<NodeId> = random_peers(&mut rng, num_peers).iter().cloned().collect();
+    block_synchronizer.register_peers(*block.hash(), peers.clone());
+
+    let events = need_next(
+        &mut rng,
+        &mock_reactor,
+        &mut block_synchronizer,
+        MAX_SIMULTANEOUS_PEERS as usize,
+    )
+    .await;
+
+    // The first thing needed after the synchronizer has peers is
+    // to fetch the block header from peers.
+    for event in events {
+        assert_matches!(
+            event,
+            MockReactorEvent::BlockHeaderFetcherRequest(FetcherRequest {
+                id,
+                peer,
+                ..
+            }) if peers.contains(&peer) && id == *block.hash()
+        );
     }
 }
